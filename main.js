3845232d449aa5d03f7e44acefef733967914aec
Malte Legenhausen <mlegenhausen@gmail.com>
2011-12-25 17:29:41 +0100

    Working version with cli

--- main.js
+++ main.js
@@ -1,41 +1,48 @@
+#!/usr/bin/env node
+
 var util = require('util');
 var net = require('net');
 var cp = require('child_process');
+var cli = require('cli');
 
 var regex = /\b(?:[0-9]{1,3}\.){3}[0-9]{1,3}\b/;
 
-function checkIp(callback) {
-	cp.exec('curl --socks5-hostname localhost:9050 http://checkip.dyndns.org/', function(err, stdout, stderr) {
+function checkIp(proxy, callback) {
+	cp.exec('curl --socks5-hostname ' + proxy + ' http://checkip.dyndns.org/', function(err, stdout, stderr) {
 		return callback(err, regex.exec(stdout).shift());
 	});
 }
 
-function run(password) {
-	var client = net.connect(9051, function() {
+function run(port, proxy, password, timeout) {
+	var client = net.connect(port, function() {
 		util.log('Connected');
 		client.write('AUTHENTICATE "' + password + '"\r\n');
 	});
-	var timeout = null;
+	var timer = null;
 	client.on('data', function(data) {
 		var result = data.toString().trim();
-		if (result !== '250 OK') util.log(result);
-		checkIp(function(err, ip) {
+		if (result !== '250 OK') return util.log(result);
+		checkIp(proxy, function(err, ip) {
 			if (err) return util.log(err);
-			util.log("Your new identity is " + ip);
+			util.log("Your identity is " + ip);
 		});
-		timeout = setTimeout(function() {
+		timer = setTimeout(function() {
 			client.write('SIGNAL NEWNYM\r\n');
-			
-		}, 10000);
+		}, timeout * 1000);
 	});
 	client.on('end', function() {
-		if (timeout) clearTimeout(timeout);
+		if (timer) clearTimeout(timer);
 		util.log('Disconnected');
 	});
 }
 
-function main(argv) {
-	run(argv.pop());
-}
+cli.parse({
+	proxy: ['x', 'Proxy address', 'string', 'localhost:9050'],
+	port: ['p', 'Control port', 'number', 9051],
+	timeout: ['t', 'Identity change interval in seconds', 'number', 10] 
+});
 
-main(process.argv);
+cli.main(function(args, options) {
+	var password = args.pop();
+	run(parseInt(options.port), options.proxy, password, parseInt(options.timeout));
+});
