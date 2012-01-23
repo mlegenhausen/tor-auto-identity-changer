9d7b006e6874fd1bde81a7eccc8baa6b2f7949a3
Malte Legenhausen <mlegenhausen@gmail.com>
2012-01-23 22:52:13 +0100

    Identity can now changed only once

--- README.md
+++ README.md
@@ -22,6 +22,12 @@ npm link
 tor-auto-identitiy-changer <password>
 ```
 
+If you only want to change the identity once, you can use:
+
+```
+tor-auto-identitiy-changer -o <password>
+```
+
 Make sure you have enabled the control port and hash authentication in the `torrc`.
 
 Use `tor-auto-identity-changer --help` for further usage informations.
--- main.js
+++ main.js
@@ -13,7 +13,7 @@ function checkIp(proxy, callback) {
 	});
 }
 
-function run(port, proxy, password, timeout) {
+function run(port, proxy, password, timeout, once) {
 	var client = net.connect(port, function() {
 		util.log('Connected to localhost:' + port);
 		client.write('AUTHENTICATE "' + password + '"\r\n');
@@ -31,25 +31,26 @@ function run(port, proxy, password, timeout) {
 		} else {
 			util.log('Successful authenticated');
 			authenticated = true;
-			util.log('Requesting new identity each ' + timeout + 's');
+			util.log('Requesting new identity' + (!once ? ' each ' + timeout + 's' : ''));
 		}
+		if (once) return client.end('SIGNAL NEWNYM\r\n');
 		timer = setTimeout(function() {
 			client.write('SIGNAL NEWNYM\r\n');
 		}, timeout * 1000);
 	});
 	client.on('end', function() {
 		if (timer) clearTimeout(timer);
-		util.log('Disconnected');
 	});
 }
 
 cli.parse({
 	proxy: ['x', 'Proxy address', 'string', 'localhost:9050'],
 	port: ['p', 'Control port', 'number', 9051],
-	timeout: ['t', 'Identity change interval in seconds', 'number', 10] 
+	timeout: ['t', 'Identity change interval in seconds', 'number', 10],
+	once: ['o', 'Change the identity only once', 'boolean', false]
 });
 
 cli.main(function(args, options) {
 	var password = args.pop();
-	run(parseInt(options.port), options.proxy, password, parseInt(options.timeout));
+	run(parseInt(options.port), options.proxy, password, parseInt(options.timeout), options.once);
 });
